35ebcffa0fc86672491c5fd6a9839ac2bb529359
Sunny Modi <sunnymodi21@gmail.com>
2019-05-17 14:20:21 -0500

    database and docker setup

--- js/database.js
+++ js/database.js
@@ -1,17 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
-// MongoClient.connect(url, {useNewUrlParser: true}, function(err, db) {
-//     if (err) throw err;
-//     var dbo = db.db("connect-4");
-//     var myobj = { name: "Company Inc", address: "Highway 37" };
-//     dbo.collection("customers").insertOne(myobj, function(err, res) {
-//       if (err) throw err;
-//       console.log("1 document inserted");
-//       db.close();
-//     });
-//   });
-
 class MongoDB{
     constructor(){
         this.db = {}
